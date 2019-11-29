8cb6ae72178bee2e8162888f2c78982849062dc8
Valentin <webkoder@ya.ru>
2019-11-29 19:10:01 +0300

    design updates

--- src/js/common.js
+++ src/js/common.js
@@ -23,3 +23,59 @@ function init(){
     myMap.geoObjects.add(myPlacemark);
 }
 
+window.document.addEventListener("DOMContentLoaded", function () { 
+
+    const anchors = [...window.document.querySelectorAll('.anchor-js')].map((anchor) => ({
+        offset: anchor.getBoundingClientRect().top + (pageYOffset || document.documentElement.scrollTop),
+        id: anchor.id
+    }));
+
+    updateMenuByHash(location.hash);
+    generateStars();
+
+    window.addEventListener("hashchange", function () {
+        updateMenuByHash(location.hash);
+    });
+
+    window.addEventListener('scroll', function (event) {        
+        const THRESHOLD = 30;
+        let newHash = '';
+        for (let i = 0; i < anchors.length; i++) {
+            if ((pageYOffset || document.documentElement.scrollTop) + THRESHOLD >= anchors[i].offset) {
+                newHash = anchors[i].id;
+            }
+        }
+        updateMenuByHash(`#${newHash}`);        
+    });
+
+    function updateMenuByHash(code) {
+        if (!code) {
+            code = '#about';
+        }
+        for (let i = 0; i < anchors.length; i++) {
+            let className = (code === `#${anchors[i].id}`) ? 'hash-menu__link hash-menu__link_active' : 'hash-menu__link';
+            window.document.querySelector(`a.hash-menu__link[href="#${anchors[i].id}"]`).className = className;
+        }
+        window.document.querySelector('div.hash-menu').className = (code === '#contacts') ? 'hash-menu hash-menu_invert' : 'hash-menu';
+    }
+
+    function generateStars() {
+        const container = window.document.querySelector('div.sky-landing__stars-container');
+        let html = '';
+        for (let i=0; i<600; i++) {
+            let left = Math.floor(Math.random()*container.clientWidth);
+            let hIndex = Math.random();
+            let top = Math.floor(hIndex*container.clientHeight);
+            let opacity = Math.floor((1 - hIndex)*6)/10;
+            html += `
+                <div class="star star_${Math.floor(Math.random()*3)}"
+                    style="top: ${top}px; left: ${left}px; opacity: ${opacity};"></div>
+            `;
+        }
+        container.innerHTML = html;
+        
+    }
+
+});
+
+
